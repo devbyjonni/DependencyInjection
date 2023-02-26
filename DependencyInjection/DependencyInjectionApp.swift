5b73cbadcbf547ede31a9fcb0ba9ce23e4cdd7ab
Jonni <enterTheSource@me.com>
2023-02-26 18:25:02 +0100

    Initial Commit

---
+++ DependencyInjection/DependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionApp.swift
+//  DependencyInjection
+//
+//  Created by Jonni Akesson on 2023-02-26.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
